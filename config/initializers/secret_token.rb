673655a4f1daa8f23a17ab1ec79f316451dee651
CASEY MCNEIL <caseymcneil@CASEYs-MacBook-Pro.local>
2014-01-11 15:56:34 -0600

    Initial commit after adding backbone/marionette/underscore js libs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackbonerailsDemo::Application.config.secret_key_base = '6343dc4721a447aeb0e7e1a9ffcf982ce6f76be603e36cf607fb0118b582dfc34a9943b7e9bf72d23e9db14dd9d128f8b8d44d8b30d056f3b6c66a9924ce8cda'
